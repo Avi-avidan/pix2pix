808dda3f8b3386ca8612b0b2e6810d011a5bd2b7
Avi Avidan <avidan2012@gmail.com>
2019-01-22 14:55:44 +0200

    colab config

---
+++ colab_config.py
@@ -0,0 +1,9 @@
+config = {}
+
+config['batch_size'] = 8
+config['thread_num'] = 1
+config['data_root'] = '/home/avi/data/train/'
+config['img_inp_shape'] = [512, 512, 1]
+config['min_size'] = 512
+config['img_pad_val'] = 0
+config['label_pad_val'] = 0
